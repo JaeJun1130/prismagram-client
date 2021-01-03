38b855882037a2dba1c5c3395a28133832255c26
재준 전 <wjswowns1234@gmail.com>
2021-01-04 04:13:23 +0900

    git test

---
+++ src/Routes/Explore/ExploreContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ExplorePresenter from "./ExplorePresenter";
+
+const ExploreContainer = () => {
+    return <ExplorePresenter />;
+};
+
+export default ExploreContainer;
---
+++ src/Routes/Explore/ExplorePresenter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExplorePresenter = () => {
+    return <div>Explore</div>;
+};
+
+export default ExplorePresenter;
