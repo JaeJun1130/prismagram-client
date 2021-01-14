5212076c1a78bd14b7d1dbac59623b5147a28efe
jaejun <wjswowns1234@gmail.com>
2021-01-15 03:32:04 +0900

    Text

--- src/Components/FatText.js
+++ src/Components/FatText.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const Text = styled.span`
   font-weight: 600;
+  font-size: ${(props) => props.size};
 `;
 
-export const FatText = ({ text }) => <Text>{text}</Text>;
+export const FatText = ({ text, size }) => <Text size={size}>{text}</Text>;
